080352f6761071fda4bd5e0980b415ee5c131ce9
ankokovin <rycarok@gmail.com>
2022-06-05 19:14:33 +0500

    added build of wasm only if environment or middleware changed (#2)
    * added build of wasm only if environment or middleware changed

--- smart-factory-wasm-port/tests/web.rs
+++ smart-factory-wasm-port/tests/web.rs
@@ -6,9 +6,9 @@ wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 pub fn it_works() {
-    let name = "World";
+    let name = "WASM";
 
     let result = smart_factory_wasm_port::greet(name);
 
-    assert_eq!("Hello, World!", result);
+    assert_eq!("Hello, WASM!", result);
 }
