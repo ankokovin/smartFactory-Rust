5f24702e80ce172c3b3f0a7629c3c11735702952
ankokovin <rycarok@gmail.com>
2022-06-05 19:50:47 +0500

    Actions builds on changes (#4)
    * fixed compilation of wasm on changes
    * also, only compile server on changes in it and environment

--- smart-factory-wasm-port/tests/web.rs
+++ smart-factory-wasm-port/tests/web.rs
@@ -6,9 +6,9 @@ wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 pub fn it_works() {
-    let name = "WASM";
+    let name = "WASM32";
 
     let result = smart_factory_wasm_port::greet(name);
 
-    assert_eq!("Hello, WASM!", result);
+    assert_eq!("Hello, WASM32!", result);
 }
